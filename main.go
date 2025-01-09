5bdf370693bd59f85be2ce530bbf97cd8107bac1
MadisonRMIT <madison.lomman@cultureamp.com>
2025-01-10 10:26:41 +1100

    add knitToBinary

--- main.go
+++ main.go
@@ -7,4 +7,16 @@ import (
 func main() {
 	content := getPatternContent("testfile.txt")
 	fmt.Println(content)
+
+	patternContent := getPatternContent("testPattern.txt")
+	patternBinary := knitToBinary(patternContent)
+	println("\n" + patternContent)
+
+	println("\n\n" + patternBinary + "\n\n")
+
+	patternContent = getPatternContent("towelPattern.txt")
+	patternBinary = knitToBinary(patternContent)
+	println("\n" + patternContent)
+
+	println("\n\n" + patternBinary)
 }
--- utils.go
+++ utils.go
@@ -4,14 +4,41 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
+const knit = 'k'
+const purl = 'p'
+
 func getPatternContent(file string) string {
+	// get the file from patterns folder & read content
 	file = filepath.Join("patterns", file)
 	content, err := os.ReadFile(file)
 	if err != nil {
 		fmt.Println("error reading pattern file: %w", err)
 		return ""
 	}
-	return string(content)
+
+	// convert byte array to string -> lowercase
+	contentStr := string(content)
+	contentStr = strings.ToLower(contentStr)
+
+	return contentStr
+}
+
+func knitToBinary(content string) string {
+	print("executing knit to binary")
+	knitBinary := ""
+	for _, char := range content {
+		if char == knit {
+			knitBinary = knitBinary + "0 "
+		} else if char == purl {
+			knitBinary = knitBinary + "1 "
+		} else if char == '\n' {
+			knitBinary = knitBinary + "\n"
+		} else {
+			continue
+		}
+	}
+	return knitBinary
 }
